1ac1ab859aaee8a6a7e85f3401c2f79e8c4de003
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-06-09 07:18:21 +0530

    First Commit: Removed default vite + react content

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App container">
+      <h1>
+        Welcome to <code>cricket-scorecard</code>!
+      </h1>
+    </div>
+  );
+}
