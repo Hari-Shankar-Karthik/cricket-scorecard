dc092ad3639de6053abe5f597a0b7b4d3f2b3fef
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-06-14 16:22:22 +0530

    Styling change to active bowler.

--- src/Bowler.jsx
+++ src/Bowler.jsx
@@ -1,15 +1,22 @@
 import convertToOvers from "./convertToOvers";
+import "./Bowler.css";
 
 export default function Bowler({ bowlerName, bowlerStats, isBowling }) {
   const { balls, maidens, runs } = bowlerStats;
-  if (balls === 0 && !isBowling) {
-    return <></>;
-  }
   const economyRate = balls === 0 ? 0 : (runs * 6) / balls;
   const overs = convertToOvers(balls);
+  if (balls !== 0 || isBowling) {
+    return (
+      <div className="Bowler active">
+        {bowlerName}
+        {isBowling && "*"} - {overs}-{maidens}-0-{runs} [ER{" "}
+        {economyRate.toFixed(1)}]
+      </div>
+    );
+  }
   return (
     <div className="Bowler">
-      {bowlerName}: {overs}-{maidens}-0-{runs} [ER {economyRate.toFixed(1)}]
+      {bowlerName} - {overs}-{maidens}-0-{runs} [ER {economyRate.toFixed(1)}]
     </div>
   );
 }
